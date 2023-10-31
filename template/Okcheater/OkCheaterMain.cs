2ae07494b04b9bf2484051becbf52f0552096335
Nightsky000 <147293897+Nightsky000@users.noreply.github.com>
2023-10-31 15:12:12 +0530

    Main

---
+++ template/Okcheater/OkCheaterMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Unity;
+using JetBrains.Annotations;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+
+namespace Okcheater
+{
+    public class OkCheaterMain : MelonMod
+    {
+        public override void OnUpdate()
+        {
+			//your code here
+        }
+    }
+}       
---
+++ template/Okcheater/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using MelonLoader;
+using OkcheaterV1;// NameSpace
+
+    [assembly: MelonInfo(typeof(OkCheaterMain), "Mod name", "Version", "Mod devloper")]
+    [assembly: MelonGame("Game std", "Game name")]
+
+
